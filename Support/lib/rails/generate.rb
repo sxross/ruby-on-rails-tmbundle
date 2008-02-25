c8d3f3a6fc7b152786a261fc09d92f5bf42b7984
Dr Nic <drnicwilliams@gmail.com>
2008-02-25 11:49:53 +1000

    'Call Generate Script' _should_ now allow you to select any available generator

--- Support/lib/rails/generate.rb
+++ Support/lib/rails/generate.rb
@@ -13,7 +13,7 @@ class Generator
   end
 
   def self.setup
-    @@list = generators
+    @@list = setup_generators
   end
 
   # Collect the names from each generator
@@ -22,12 +22,25 @@ class Generator
   end
 
   def self.generators
-    known_generators
+    @@list
+  end
+
+  def self.setup_generators
+    known_generator_names = known_generators.map { |gen| gen.name }
+    new_generator_names = find_generator_names - known_generator_names
+    known_generators + new_generator_names.map do |name|
+      Generator[name, "Arguments for #{name} generator:", ""]
+    end
   end
 
   # Runs the script/generate command and extracts generator names from output
   def self.find_generator_names
-    []
+    list = nil
+    FileUtils.chdir(RailsPath.new.rails_root) do
+      output = ruby 'script/generate | grep "^  [A-Z]" | sed -e "s/  //"'
+      list = output.split.reject {|f| f =~ /:/}
+    end
+    list
   end
 
   def self.known_generators
--- Support/test/test_helper.rb
+++ Support/test/test_helper.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require "fileutils"
 $:.push File.dirname(__FILE__)
 $:.push File.dirname(__FILE__) + '/../lib'
 FIXTURE_PATH = File.expand_path(File.dirname(__FILE__) + '/app_fixtures')
